71488ce113a23be659d08e126b1a6d8039fa7914
shir-yehuda <82862990+shir-yehuda@users.noreply.github.com>
2021-07-08 17:36:38 +0300

    rendered hardcoded counter in mainview (#1)

---
+++ src/packages/Counter/components/Counter/CounterPureComponent.tsx
@@ -0,0 +1,10 @@
+import style from "../../counterSytle.module.css"
+
+export const CounterPureComponent=()=>{
+   return  <div className={style.counter}>
+        <div>
+            this is my counter
+            5
+        </div>
+    </div>
+}
--- src/packages/TopBar/component/TopBarComponentPure.tsx
+++ src/packages/TopBar/component/TopBarComponentPure.tsx
@@ -1,3 +1,4 @@
+import '../topBarStyle.css';
 
 
 export const TopBarComponentPure = () => (
