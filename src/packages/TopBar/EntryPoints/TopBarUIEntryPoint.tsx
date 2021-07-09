c3efec6ea5ec0e22597a9e8b2291340b46f5028d
shir-yehuda <82862990+shir-yehuda@users.noreply.github.com>
2021-07-09 15:13:49 +0300

    Create topbar api (#2)
    * created top bar api

--- src/packages/TopBar/EntryPoints/TopBarUIEntryPoint.tsx
+++ src/packages/TopBar/EntryPoints/TopBarUIEntryPoint.tsx
@@ -1,13 +1,18 @@
 import {EntryPoint, Shell, SlotKey} from "repluggable";
 import {MainViewAPI} from "../../mainView";
 import {TopBarComponentPure} from "../component/TopBarComponentPure";
+import {createTopBarAIP, TopBarAPI, TopBarAPISlotKey} from "../Api/TopBarAPI";
 
 export const TopBarUIEntryPoint:EntryPoint={
     name:"Top Bar",
     getDependencyAPIs(): SlotKey<any>[] {
         return [MainViewAPI]
     },
+    declareAPIs() {
+        return [TopBarAPISlotKey]
+    },
     attach(shell: Shell) {
+        shell.contributeAPI(TopBarAPISlotKey,()=>createTopBarAIP(shell))
 
     },
     extend(shell: Shell) {
