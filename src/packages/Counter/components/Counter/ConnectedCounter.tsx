757c042d34a4aa18de9aeff16b582d75ea8f9cbd
Shir Yehuda <shiry@wix.com>
2021-07-19 14:40:24 +0300

    added connected counter

---
+++ src/packages/Counter/components/Counter/ConnectedCounter.tsx
@@ -0,0 +1,26 @@
+import {connectWithShell, ExtensionSlot, ReactComponentContributor, Shell} from "repluggable";
+
+import {CounterPureComponent} from "./CounterPureComponent";
+
+// const mapDispatchToProps = (shell: Shell): ZoomBarDispatchProps => {
+//
+//     return {
+//
+//     };
+// };
+//
+// export interface TopBarStateProps{
+//     slot:ExtensionSlot<any>
+// }
+// const mapStateToProps = (shell: Shell): TopBarStateProps => {
+//     const topBarApi=shell.getAPI(TopBarAPI)
+//
+//     return {
+//         slot:topBarApi.getSlot()
+//     };
+// };
+
+
+export const createConnectedTopBar=(boundShell:Shell)=>{
+    return connectWithShell<{},{},{},{}>(undefined,undefined,boundShell)(CounterPureComponent)
+}
